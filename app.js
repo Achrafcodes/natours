a9d1cfa7a102bbb31aa2c9d34d6034ce2f293ccf
Achrafcodes <essoussygamer@gmail.com>
2023-10-23 15:18:28 +0100

    natours

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const port = 3000;
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'hello from express', app: 'natour' });
+  res.status(200).json({
+    name: 'first express code',
+    message: 'first object in next',
+  });
 });
-
+app.post('/', (req, res) => {
+  res.send('you can post anythig here ');
+});
+const port = 3000;
 app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+  console.log(` app is running in port ${port}`);
 });
